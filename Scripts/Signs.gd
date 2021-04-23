2719544348db213e67dcec9ddcb2a8066e8b5a32
MyNameIsntBob <Thoseoneguys5250@gmail.com>
2021-04-22 21:01:52 -0600

    made the title screen look better

--- Scripts/Signs.gd
+++ Scripts/Signs.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var drop_time := 1.0
+export var drop_time := 2.0
 export var raise_time := 1.0
 
 var raise_to
@@ -15,15 +15,30 @@ func _ready():
 	drop()
 
 func drop():
-	print("Dropping")
 	$Tween.interpolate_property($Mount, "position", $Mount.position, Vector2(0, drop_to), drop_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
-	print('Dropped')
 #	yield($Tween, "tween_completed")
 #	emit_signal('finished')
 	
 func raise():
-	$Tween.interpolate_property($Mount, "position", $Mount.position, Vector2(0, raise_to), raise_time, Tween.TRANSLINEAR, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Mount, "position", $Mount.position, Vector2(0, raise_to), raise_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
-#	yield($Tween, "tween_completed")
 #	emit_signal('finished')
+
+
+func _on_Play_pressed():
+	raise()
+	$Chain3/Join1.queue_free()
+	$Chain4/Join1.queue_free()
+	yield($Tween, "tween_completed")
+	Global.load_scene('connect')
+#	print('Play')
+	pass # Replace with function body.
+
+
+
+func _on_Options_pressed():
+	Global.load_scene('settings')
+
+func _on_Exit_pressed():
+	get_tree().quit()
