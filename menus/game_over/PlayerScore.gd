5538a3e66b3c6e67596a19930d89ee4fe885e45e
MyNameIsntBob <37026012+MyNameIsntBob@users.noreply.github.com>
2022-01-23 07:02:45 -0700

    minor fixes

--- menus/game_over/PlayerScore.gd
+++ menus/game_over/PlayerScore.gd
@@ -4,7 +4,7 @@ export var player_id : int
 
 var scores_to_text := ['1st\n', '2nd\n', '3rd\n', '4th\n']
 
-func _ready():
+func set_score():
 	var character_id = ScoreKeeper.get_character_id(player_id)
 	if character_id == null:
 		return
@@ -15,3 +15,4 @@ func _ready():
 	find_node('Scores').text = score + kills + deaths
 	
 	$Image.texture = Icons.get_gui_character(character_id)
+	
